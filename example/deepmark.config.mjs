604b5c07be44519e4ddd0e99023d362816613932
Izzuddin Natsir <izznatsir@gmail.com>
2022-09-25 10:12:30 +0700

    chore: fix example deepmark config

--- example/deepmark.config.mjs
+++ example/deepmark.config.mjs
@@ -4,8 +4,8 @@ export default {
 	outputLanguages: ['zh', 'ja'],
 	directories: [
 		['i18n/$langcode$', 'i18n/$langcode$'],
-		['docs', 'i18n/$langcode$/docs'],
-		['blog', 'i18n/$langcode$/blog']
+		['docs', 'i18n/$langcode$/docusaurus-plugin-content-docs/current'],
+		['blog', 'i18n/$langcode$/docusaurus-plugin-content-blog']
 	],
 	jsonOrYamlProperties: {
 		include: ['message', 'description']
